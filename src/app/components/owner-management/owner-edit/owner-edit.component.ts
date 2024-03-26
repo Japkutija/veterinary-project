061e8087af945fbcd8d3717ffc4eec368b159140
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-03-26 14:40:10 +0100

    - Structured owner management component with owner list table with example data, - Added metadata to owner-management component, - Prepared action buttons for future modal integration.

---
+++ src/app/components/owner-management/owner-edit/owner-edit.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-edit',
+  templateUrl: './owner-edit.component.html',
+  styleUrls: ['./owner-edit.component.css']
+})
+export class OwnerEditComponent {
+
+}
---
+++ src/app/components/owner-management/owner-management.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-owner-management',
+  templateUrl: './owner-management.component.html',
+  styleUrls: ['./owner-management.component.css']
+})
+export class OwnerManagementComponent {
+
+}
