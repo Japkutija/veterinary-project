f4eab1879f090e033f67459c880080c7a0ce0d80
Japkutija <126104434+Japkutija@users.noreply.github.com>
2024-09-03 19:33:19 +0200

    Integrated NG-ZORRO and refactoedr layout
    - Added NG-ZORRO library and related Angular modules for UI enhancements.
    - Refactored the application layout using NG-ZORRO components, including header, content, and footer sections.
    - Updated styles to align with NG-ZORRO design language.
    - Replaced custom HTML and CSS in the home component with NG-ZORRO layout components.
    - Adjusted project configuration to include additional assets and styles for NG-ZORRO.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,23 @@ import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
 import { PetManagementComponent } from './components/pet-management/pet-management.component';
 import { PetEditComponent } from './components/pet-management/pet-edit/pet-edit.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { sl_SI } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import sl from '@angular/common/locales/sl';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzTypographyModule } from 'ng-zorro-antd/typography';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { HeaderComponent } from './components/header/header/header.component';
+
+
+registerLocaleData(sl);
 
 @NgModule({
   declarations: [
@@ -15,12 +32,24 @@ import { PetEditComponent } from './components/pet-management/pet-edit/pet-edit.
     LoginComponent,
     PetManagementComponent,
     PetEditComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzMenuModule,
+    NzButtonModule,
+    NzTypographyModule,
+    NzListModule
   ],
-  providers: [],
+  providers: [
+    { provide: NZ_I18N, useValue: sl_SI }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
